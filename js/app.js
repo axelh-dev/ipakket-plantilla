1666834b66434b94a2cbec46fd41a284fdc286d2
axelPrac <axelherreram7@gmail.com>
2024-03-11 11:05:10 -0600

    cambios en la navbar

--- js/app.js
+++ js/app.js
@@ -1,62 +1,67 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  // Función para verificar la visibilidad de los elementos
+  function checkVisibility() {
     var items = document.querySelectorAll(".item");
-  
-    function checkVisibility() {
-      // Comprobar visibilidad de los elementos .item
-      items.forEach(function (item) {
-        var position = item.getBoundingClientRect();
-  
-        // Si el objeto está dentro del viewport
-        if (position.top < window.innerHeight * 0.8) {
-          item.classList.add("visible");
-        }
-      });
-      
-    }
-  
-    // Comprueba la visibilidad al cargar la página y al hacer scroll
-    checkVisibility();
-    window.addEventListener("scroll", checkVisibility);
-  
-    // Resto del código...
-    var navbarShrink = function () {
-      const navbarCollapsible = document.body.querySelector("#mainNav");
-      if (!navbarCollapsible) {
-        return;
+    items.forEach(function (item) {
+      var position = item.getBoundingClientRect();
+      if (position.top < window.innerHeight * 0.8) {
+        item.classList.add("visible");
       }
-      if (window.scrollY === 0) {
-        navbarCollapsible.classList.remove("navbar-shrink");
+    });
+  }
+  checkVisibility();
+
+  // Función para cambiar el color de la barra de navegación
+  function changeNavbarColor() {
+    var navbar = document.querySelector(".navbar");
+    var aboutSection = document.getElementById("section-about");
+
+    if (aboutSection) {
+      var scroll = window.scrollY || window.pageYOffset;
+      var aboutSectionTop = aboutSection.offsetTop;
+      var navbarHeight = navbar.offsetHeight;
+
+      if (scroll > aboutSectionTop - navbarHeight) {
+        navbar.style.backgroundColor = "rgb(43,48,53)";
       } else {
-        navbarCollapsible.classList.add("navbar-shrink");
+        navbar.style.backgroundColor = "transparent";
       }
-    };
-  
-    // Shrink the navbar
-    navbarShrink();
-  
-    // Shrink the navbar when page is scrolled
-    document.addEventListener("scroll", navbarShrink);
-  
-    //  Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector("#mainNav");
-    if (mainNav) {
-      new bootstrap.ScrollSpy(document.body, {
-        target: "#mainNav",
-        rootMargin: "0px 0px -40%",
-      });
+    } else {
+      console.log("Elemento #section-about no encontrado");
     }
-  
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector(".navbar-toggler");
-    const responsiveNavItems = [].slice.call(
-      document.querySelectorAll("#navbarResponsive .nav-link")
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-      responsiveNavItem.addEventListener("click", () => {
-        if (window.getComputedStyle(navbarToggler).display !== "none") {
-          navbarToggler.click();
-        }
-      });
+  }
+
+
+
+  // Inicialización de Bootstrap ScrollSpy
+  const mainNav = document.body.querySelector("#mainNav");
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#mainNav",
+      rootMargin: "0px 0px -40%",
+    });
+  }
+
+  // Cierre del menú desplegable al hacer clic en un enlace (responsive)
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
     });
   });
-  
+
+  // Event Listeners
+  window.addEventListener("scroll", changeNavbarColor);
+  window.addEventListener("scroll", checkVisibility);
+  document.addEventListener("scroll", navbarShrink);
+
+  // Ejecutar funciones al cargar la página
+
+  changeNavbarColor();
+  navbarShrink();
+});
